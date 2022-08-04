4509e5f60d88d1b9fa5b3101a1a0cd2b87393751
achillesthree <105816630+achillesthree@users.noreply.github.com>
2022-08-04 03:13:16 -0400

    Update script.js
    still with more comments to finish but almost there

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
-// Assignment Code
+// variable to set the min characters and a var that creates and "any" arr which will be completed by user
 var characterLength = 8;
 var choiceArr = [];
 
+// variables to name our arrays (strings) which hold and corspond to all of the possible user selection criteria 
 var specialCharArr = ['!', '@', '#', '$', '%', '^','&','*','<','>','?','/','+',];
 var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
 var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 var numberArr = ['0','1','2','3','4','5','6','7','8','9',]; 
 
-
+// variable to make a button to generate password with a click event that will write PW for user
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 
-// Write password to the #password input
+// function to write a password with variables to recognize correct prompts to get those prompts
 function writePassword() {
   var correctPrompts = getPrompts();
+  // fumction to write a pasword text based on the DQS "password"
   var passwordText = document.querySelector("#password");
 
+  // if statement- if the correct prompts gets/cleears/accepts? NPW and GPW then to the PW text value and THAT will be the NPW 
   if (correctPrompts) {
     var newPassword = generatePassword ();
     passwordText.value = newPassword;
+
+    // else (not everything goes through correctly) then an empty password text VALUE
   } else {
     passwordText.value = "";
   }
-
-  //WHEN the password is generated THEN the password is either displayed in an alert or written to the page
-// 
 }
+
+// function to genetrate password with var password to equal user PW incorperating a for loop until specifications are met
 function generatePassword() {
   var password = "";
   for(var i = 0; i < characterLength; i++) {
+    // used var randomindex with math floor to make integers and a random open () multiplied by user choice of length 
     var randomIndex = Math.floor(Math.random() * choiceArr.length);
     password = password + choiceArr[randomIndex];
   }
+  // password is returned if all criteria met
   return password;
 }
 
+// funtion to get prompts based on the choice array which was made empty up top
 function getPrompts() {
   choiceArr = [];
-
+// this was named above in or for loop and parseInt to return a string as a number (for number of characters)
   characterLength = parseInt(prompt("How many characters between 8 - 128 would you like your PW 2B?")); 
-
+  // this function of not a number is a check on the criteria if it's NOT the right less than 8 OR greater than 128 it will return as false and display the alert text in orange and quotes 
   if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert("Characters MUST be a number, at least 8 characters and less than 128 characters. Try again.");
     return false;
@@ -61,21 +68,5 @@ if (confirm("Numbers in your PW?")) {
 }
 return true;
 }
-  // THEN I am presented with a series of prompts for password criteria WHEN prompted for password criteria
-  // look up window.Prompt
   
-  //  WHEN prompted for password criteria THEN I select which criteria to include in the password 
-  // save each window prompt as VAR
-
-  // WHEN prompted for the length of the password THEN I choose a length of at least 8 characters and no more than 128 characters
-  // Window.Prompt to ask the user the length of the password "how long do yuo want your password? User enter number- saving this vaule as a VAR"
-
-  // WHEN asked for character types to include in the password THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// Window.Confirm for each charchter type (four different window confirms lower case upper case numbers and special charc- save as VARS as well
-
-//WHEN I answer each prompt THEN my input should be validated and at least one character type should be selected
-// Check at least one WC is true (that's why were saving as VARS)
-
-//WHEN all prompts are answered THEN a password is generated that matches the selected criteria
-//biggest part of function- concat user selections to the possible charcatres array (that array starts out as empty)
 
